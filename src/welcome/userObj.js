62cf37ea79a8777d4b24a20392b648cfa0cf035f
Allison Johanson <67883578+AllyJoho@users.noreply.github.com>
2025-10-22 22:12:00 -0600

    making objects and stuff trying to make it all work

---
+++ src/welcome/userObj.js
@@ -0,0 +1,15 @@
+function getAge(name) {
+  // This will be replaced with a 3rd party service call
+  return Math.floor(Math.random() * 30) + 15 + name.length;
+}
+
+export class Users {
+    constructor(name, username, password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.games = [];
+        this.friends = [];
+        this.age = getAge(name);
+    }
+}
